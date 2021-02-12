0ca91274508c07e3e06ecee91f0d0e63f4724dd8
Rgux <flxwillrgux@gmail.com>
2021-02-12 21:37:31 +0700

    Added Camera Follow, Tweaks
    CameraFollow (Camera Will Follow Char Only)
    CollisionWeapon (Ignore terrain)
    Movement Improved (Give Momentum, Halt Velocity)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+	public float cameraDistance = 30f;
+
+	void Awake()
+	{
+		GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+	}
+	
+	void FixedUpdate()
+	{
+		transform.position = new Vector3(player.position.x, player.position.y, -5);
+	}
+}
